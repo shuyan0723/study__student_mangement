3d9f9ef0dfab360d6d92160f57f998f37b0d89e6
Vercel <vercel[bot]@users.noreply.github.com>
2026-03-01 01:26:38 +0000

    Set up Vercel Speed Insights tracking
    This is an automated pull request generated by Vercel to help install [Speed Insights](https://vercel.com/docs/speed-insights) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- student-study/src/App.tsx
+++ student-study/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import './App.css';
 import AppRoutes from './routes';
 
@@ -9,6 +10,7 @@ function App() {
     <ConfigProvider locale={zhCN}>
       <Router>
         <AppRoutes />
+        <SpeedInsights />
       </Router>
     </ConfigProvider>
   );
